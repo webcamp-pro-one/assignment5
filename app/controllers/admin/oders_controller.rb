33545f6e6bd5000040aecda67e91e46a04b80c91
EC2 Default User <ec2-user@ip-172-31-52-204.ec2.internal>
2021-03-06 11:02:13 +0000

    add controllers

---
+++ app/controllers/admin/oders_controller.rb
@@ -0,0 +1,7 @@
+class Admin::OdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,55 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'oders/index'
+    get 'oders/show'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :admins
-  devise_for :customers
+
+  devise_for :customers, controllers: {
+    registration: 'public/ragistration',
+    session: 'public/sessions'
+  }
+  
+  devise_scope :customer do 
+    get 'customer/sign_up' => 'public/registrations#new'
+    get 'customer/sign_in' => 'public/sessions#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
